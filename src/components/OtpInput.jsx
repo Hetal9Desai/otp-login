58323f2631a95def2a734cdb9d563644caba9d8d
Hetal Desai <hetal.desai@simformsolutions.com>
2025-05-09 14:32:39 +0530

    Add : Add handleClick and handleKeyDown functionality

--- src/components/OtpInput.jsx
+++ src/components/OtpInput.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
-const OtpInput = (length = 4, onOtpSubmit = () => {}) => {
+const OtpInput = ({ length = 4, onOtpSubmit = () => {} }) => {
   const [otp, setOtp] = useState(new Array(length).fill(""));
-  console.log(otp);
+  const inputRefs = useRef([]);
 
   useEffect(() => {
     if (inputRefs.current[0]) {
@@ -27,25 +27,43 @@ const OtpInput = (length = 4, onOtpSubmit = () => {}) => {
     }
   };
 
-  const handleClick = () => {};
-  const handlekeyDoen = () => {};
-  return otp.map((value, index) => {
-    return (
-      <input
-        key={index}
-        type="text"
-        value={value}
-        onChange={(e) => handleChange(index, e)}
-        onClick={() => {
-          handleClick(index);
-        }}
-        onkeydown={(e) => {
-          handlekeyDoen(index, e);
-        }}
-        className="otpInput"
-      />
-    );
-  });
+  const handleClick = (index) => {
+    inputRefs.current[index].setSelectionRange(1, 1);
+
+    if (index > 0 && !otp[index - 1]) {
+      inputRefs.current[otp.indexOf("")].focus();
+    }
+  };
+
+  const handleKeyDown = (index, e) => {
+    if (
+      e.key === "Backspace" &&
+      !otp[index] &&
+      index > 0 &&
+      inputRefs.current[index - 1]
+    ) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  return (
+    <div>
+      {otp.map((value, index) => {
+        return (
+          <input
+            key={index}
+            type="text"
+            ref={(input) => (inputRefs.current[index] = input)}
+            value={value}
+            onChange={(e) => handleChange(index, e)}
+            onClick={() => handleClick(index)}
+            onKeyDown={(e) => handleKeyDown(index, e)}
+            className="otpInput"
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default OtpInput;
