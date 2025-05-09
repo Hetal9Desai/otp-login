ce83283909c9166fc9cd25315ac36fd3a4212842
Hetal Desai <hetal.desai@simformsolutions.com>
2025-05-09 13:42:10 +0530

    Add : Add handleChange functionality

--- src/components/OtpInput.jsx
+++ src/components/OtpInput.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const OtpInput = (length = 4, onOtpSubmit = () => {}) => {
   const [otp, setOtp] = useState(new Array(length).fill(""));
   console.log(otp);
 
-  const handleChange = () => {};
+  useEffect(() => {
+    if (inputRefs.current[0]) {
+      inputRefs.current[0].focus();
+    }
+  }, []);
+
+  const handleChange = (index, e) => {
+    const value = e.target.value;
+    if (isNaN(value)) return;
+
+    const newOtp = [...otp];
+
+    newOtp[index] = value.substring(value.length - 1);
+    setOtp(newOtp);
+
+    const combinedOtp = newOtp.join("");
+    if (combinedOtp.length === length) onOtpSubmit(combinedOtp);
+
+    if (value && index < length - 1 && inputRefs.current[index + 1]) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
   const handleClick = () => {};
   const handlekeyDoen = () => {};
   return otp.map((value, index) => {
